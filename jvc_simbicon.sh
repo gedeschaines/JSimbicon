df7b286eb6868875bc21b41dd6e023a96015cb74
Gary Deschaines <gedeschaines@gmail.com>
2022-12-09 06:34:26 -0600

    Added mkdir ./classes check.

--- jvc_simbicon.sh
+++ jvc_simbicon.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -e ./classes ]
+then
+  mkdir ./classes
+fi
 export JAVA_HOME=${JAVA8_HOME}
 export PATH=${JAVA_HOME}/bin:${PATH}
 ${JAVA_HOME}/bin/javac ./src/*.java -d ./classes
